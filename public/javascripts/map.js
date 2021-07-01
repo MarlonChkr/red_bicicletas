72d90765d364cbe11ffde1a2502e4f5ed8b3131f
MarlonChkr <marlon.chica@elite.edu.co>
2021-07-01 18:19:04 -0300

    agregando template

---
+++ public/javascripts/map.js
@@ -0,0 +1,9 @@
+    var map = L.map('main_map').setView([-34.6012424,-58.3861497],13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    }).addTo(map);
+    
+    L.marker([-34.6012424, -58.3861497]).addTo(map);
+    L.marker([-34.606932, -58.3868287]).addTo(map);
+    L.marker([-34.599564, -58.3778777]).addTo(map);
